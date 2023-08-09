f236cd3044f8cb83d2b6dd9ed4a4a97013738ca7
Kevin Ulrich <kzulrich@gmail.com>
2023-08-09 14:26:47 -0700

    fix relative pathing of windows scripts

--- scripts/windows/run.ps1
+++ scripts/windows/run.ps1
@@ -1,3 +1,3 @@
 echo "Starting server and client"
-invoke-expression 'cmd /c start powershell -Command { cd server; yarn start }'
-invoke-expression 'cmd /c start powershell -Command { cd client; yarn start }'
+invoke-expression 'cmd /c start powershell -Command { cd ../../server; yarn start }'
+invoke-expression 'cmd /c start powershell -Command { cd ../../client; yarn start }'
--- scripts/windows/setup.ps1
+++ scripts/windows/setup.ps1
@@ -1,3 +1,3 @@
 echo "Installing client and server packages"
-invoke-expression 'cmd /c start powershell -Command { cd client; yarn }'
-invoke-expression 'cmd /c start powershell -Command { cd server; yarn }'
+invoke-expression 'cmd /c start powershell -Command { cd ../../client; yarn }'
+invoke-expression 'cmd /c start powershell -Command { cd ../../server; yarn }'
