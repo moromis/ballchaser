0ab2263b685064a8925ad940cfd9cdb527775ce0
Kevin Ulrich <kzulrich@gmail.com>
2023-08-09 13:29:19 -0700

    add windows scripts

---
+++ scripts/windows/run.ps1
@@ -0,0 +1,3 @@
+echo "Starting server and client"
+invoke-expression 'cmd /c start powershell -Command { cd server; yarn start }'
+invoke-expression 'cmd /c start powershell -Command { cd client; yarn start }'
---
+++ scripts/windows/setup.ps1
@@ -0,0 +1,3 @@
+echo "Installing client and server packages"
+invoke-expression 'cmd /c start powershell -Command { cd client; yarn }'
+invoke-expression 'cmd /c start powershell -Command { cd server; yarn }'
